dfc3914038d6d991f460669ffbd51f4e9f2376f1
jsjohn1951 <jsjohn1951@users.noreply.github.com>
2023-07-24 18:59:02 +0400

    first commit

---
+++ scripts/clientInit.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+
+printf "Running client locally? "
+tput setaf 1
+printf "y/n "
+tput init
+printf ": "
+read lclient
+
+printf "\n"
+x=1
+
+if [ "${lclient}" = "y" ];
+then
+	make re -C ./client
+	clear
+	tput setaf 2
+	printf "Attempting to init client...\n"
+	tput init
+
+	printf "This may take a while.\n"
+	printf "Waiting for server "
+	while [ $x = 1 ];
+	do
+	if [ -f server/server/server.log ];
+	then
+		clear
+		tput setaf 2
+		printf "Server found!\n"
+		tput init
+		./client/client 127.0.0.1 6667 wismith bob
+		make fclean
+		exit 0
+	else
+		tput setaf 2
+		printf " . "
+		tput init
+		sleep 1
+	fi
+done
+
+else
+	clear
+	printf "Nothing to see here folks!\n\n"
+	printf "You must run the client manually from remote machine.\n"
+	printf "For Remote Client :\n"
+	printf "\tUse host's IPv4 -> \""
+	tput setaf 2
+	ipconfig getifaddr en0 | tr -d '\n'
+	tput init
+	printf "\"\n\tUse host's Port -> \""
+	tput setaf 1
+	printf "6667"
+	tput init
+	printf "\"\n"
+	printf "\tUse host's Password -> \""
+	tput setaf 5
+	printf "bob"
+	tput init
+	printf "\"\n"
+fi
